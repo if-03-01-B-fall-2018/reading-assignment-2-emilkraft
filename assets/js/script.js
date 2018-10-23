d7900dded0d69cf9083b599995c1267697ac7da4
Emil Kraft <emil.kraft@outlook.at>
2018-10-23 08:13:12 +0200

    [+] Updated CheatSheet

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) 2018 Emil Kraft. All Rights Reserved.
+ */
+$(function() {
+    $('a[href*=#]').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    });
+});
